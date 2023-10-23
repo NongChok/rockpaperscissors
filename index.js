819c95c423491100e598e01b78012e4f39a89a07
NongChok <bkkquilt@github.leemail.me>
2023-10-23 14:56:51 +0700

    Write a function that plays a single round of game

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
+    
     if(randomNumber == 0) {
-        computerChoice = 'rock';
+        return 'rock';
     }
     if(randomNumber == 1) {
-        computerChoice = 'paper';
+        return 'paper';
         }
     if(randomNumber == 2) {
-        computerChoice = 'scissors';
+        return 'scissors';
     }
 }
-getComputerChoice();
-console.log("The computer chooses", computerChoice);
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return 'It is a tie';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "scissors") {
+        return "You win, rock breaks scissors";
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "rock") {
+        return "You win, paper covers rock";
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "paper") {
+        return "You win, scissors cuts paper";
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "paper") {
+        return "Computer wins, paper covers rock";
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "scissors") {
+        return "Computer wins, scissors cut paper";
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "rock") {
+        return "Computer wins, rock breaks scissors";}
+
+  }
+   
+   const playerSelection = "rock";
+   const computerSelection = getComputerChoice();
+    
+    console.log("You chose rock. The computer chose", computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
